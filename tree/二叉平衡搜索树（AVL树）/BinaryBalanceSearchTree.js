897c9894c7e7e03d80e859a75482bea6bcbc2be1
yangyang01 <yangyang01@iyunxiao.com>
2020-12-12 12:33:04 +0800

    平衡二叉搜索树的节点高度计算公式的原理-done

--- tree/二叉平衡搜索树（AVL树）/BinaryBalanceSearchTree.js
+++ tree/二叉平衡搜索树（AVL树）/BinaryBalanceSearchTree.js
@@ -73,13 +73,23 @@ class BinaryBalanceSearchTree {
 
     // 计算节点高度
     getNodeHeight(node) {
+        /**
+         * 所以以这种层层递归的方式计算A树的高度，最终会遍历到计算叶子节点的高度。因为叶子节点的左孩子和右孩子均为空的，所以都返回了-1，叶子节点的高度就为Math.max(-1, -1) + 1 = 0了。
+         */
         if (node === null) {
             return -1;
         }
+        /**
+         * 解释为什么是：Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) + 1
+         * 举例子：假如有一个数树，根节点为A，左孩子为B，右孩子为C
+         * 要计算这个树的高度：只需要计算以节点B为根节点的子树的高度（假如高度为b）和以节点C为根节点的子树的高度（假如高度为c），求出Math.max(b, c)的最大值，然后再加上节点A本身就占据一层的高度，所以最后需要再加上1（把节点A加入到层高的一部分）
+         * 
+         * 所以以这种层层递归的方式计算A树的高度，最终会遍历到计算叶子节点的高度。因为叶子节点的左孩子和右孩子均为空的，所以都返回了-1，叶子节点的高度就为Math.max(-1, -1) + 1 = 0了。
+         */
         return Math.max(this.getNodeHeight(node.left), this.getNodeHeight(node.right)) + 1;
     }
 
-    // 获取平衡因子
+    // 获取平衡因子（注意：不要把节点高度和节点的平衡因子搞混了，节点高度考虑的是节点最长的那一条边的高度，平衡因子是节点左右两棵树的高度差）
     getBalanceFactor(node) {
         let heightDifference = this.getNodeHeight(node.left) - this.getNodeHeight(node.right);// 高度差
 
